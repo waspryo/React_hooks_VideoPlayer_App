a3670bf78ed8142339c73c7e5f2c82d4e3a0c09e
waspryo <wasp2123321@gmail.com>
2019-09-18 19:19:53 +0700

    create playlist and players

---
+++ src/components/containers/Playlist.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PlaylistHeader from "../PlaylistHeader";
+import PlaylistItems from "../containers/PlaylistItems";
+import Nightmode from "../Nightmode";
+
+const Playlist = props => (
+  <>
+    <Nightmode />>
+    <PlaylistHeader />>
+    <PlaylistItems />>
+  </>
+);
+
+export default Playlist;
