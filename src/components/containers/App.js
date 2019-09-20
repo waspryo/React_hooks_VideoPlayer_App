d47ba0ca3987e5434ab00748d9236ad623e31e02
waspryo <wasp2123321@gmail.com>
2019-09-20 20:49:12 +0700

    add styleComponents

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import WbnPlayer from './WbnPlayer'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import GlobalStyle from '../styles/GlobalStyle'
 
 const App = () => (
   <BrowserRouter>
@@ -8,6 +9,7 @@ const App = () => (
       <Route exact path="/" component={WbnPlayer}/>
       <Route exact path="/:activeVideo" component={WbnPlayer}/>
     </Switch>
+    <GlobalStyle/>
   </BrowserRouter>
 )
 
