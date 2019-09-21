a63b1d9364dbb3a31e5519b0c9f4e211e5894a6e
waspryo <wasp2123321@gmail.com>
2019-09-21 14:48:13 +0700

    add NightmodePlayer props and Wbn styling

--- src/components/Nightmode.js
+++ src/components/Nightmode.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import StyledNightmode from "./styles/StyledNightmode";
 
-const NightMode = props => {
-    return (
-        <div>
-            NightMode!
-        </div>
-    )
-}
+const NightMode = ({NightModeCallback, nightMode}) => {
+  return (
+    <StyledNightmode>
+      <span>NightMode: </span>
+      <label className="switch">
+        <input
+          type="checkbox"
+          checked={nightMode}
+          onChange={NightModeCallback}
+        />
+        <span className="slider round"/>
+      </label>
+    </StyledNightmode>
+  );
+};
 
 export default NightMode;
