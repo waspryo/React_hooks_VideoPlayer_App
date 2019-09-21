c023281cab54f70fffa5af74c7a6e64bf5eb676f
waspryo <wasp2123321@gmail.com>
2019-09-21 18:34:21 +0700

    styling video

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import ReactPlayer from 'react-player'
+import React from "react";
+import ReactPlayer from "react-player";
+import StyledVideoWrapper from "./styles/StyledVideoWrapper";
+import StyledVideo from "./styles/StyledVideo";
 
-const Video = props => {
-    return (
-        <div>
-            Video!
-        </div>
-    )
-}
+const Video = ({ active, autoplay, endCallback, progressCallback }) => {
+  return (
+    <StyledVideo>
+      <StyledVideoWrapper>
+          <ReactPlayer
+            width="100%"
+            height="100%"
+            style={{ position: "absolute", top: "0" }}
+            playing={autoplay}
+            controls={true}
+            url={active.video}
+            onEnded={endCallback}
+            onProgress={progressCallback}
+          />
+      </StyledVideoWrapper>
+    </StyledVideo>
+  );
+};
 
 export default Video;
