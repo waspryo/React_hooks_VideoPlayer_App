24eff90f7864d53fbed37ef83f5c77fb9894eebc
waspryo <wasp2123321@gmail.com>
2019-09-25 23:36:33 +0900

    Updates

---
+++ precache-manifest.a09553ae1ad2bf116c79b5ae70400fb5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e1390872954c5135e05d76cc560d51",
+    "url": "/react_videoplayer/index.html"
+  },
+  {
+    "revision": "67433127310482539e28",
+    "url": "/react_videoplayer/static/js/2.fdb6107a.chunk.js"
+  },
+  {
+    "revision": "f796a4aac48f7b1da707",
+    "url": "/react_videoplayer/static/js/main.11178b68.chunk.js"
+  },
+  {
+    "revision": "9d808bd6d5131ace177f",
+    "url": "/react_videoplayer/static/js/runtime~main.08c55151.js"
+  }
+]);
